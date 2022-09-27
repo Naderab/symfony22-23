42853bd996b66f0636362ae902e87095c4586da1
Naderab <abdellaouinader@gmail.com>
2022-09-27 12:01:10 +0100

    twig part2

---
+++ Project3A33/src/Controller/ClubController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClubController extends AbstractController
+{
+    #[Route('/club', name: 'app_club')]
+    public function index(): Response
+    {
+        return $this->render('club/index.html.twig', [
+            'controller_name' => 'ClubController',
+        ]);
+    }
+    #[Route('/club/get/{nom}', name: 'app_club_getName')]
+    public function getName($nom):Response {
+return $this->render('club/detail.html.twig', ['nom'=>$nom]);
+    }
+}
